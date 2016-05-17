c2b3ef22bf1b0070dad10a4ff9c86bf72bb2dd5b
Joshua Matsuoka <Jmatsuok@unused-10-15-17-189.yyz.redhat.com>
2016-05-17 10:49:15 -0400

    Added argument checks
    If the program receives more than one argument it returns 1

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
   #include <stdio.h>
 
-  int main(){
-      printf("goodbye world :(\n");
-      return 1+3;
+  int main(int argc, char* argv){
+      if(argc > 1) {
+  	  return 0;
+      }
+      else {
+         printf("Hello");
+         printf("goodbye world :(\n");
+         return -1;
+      }
   }
